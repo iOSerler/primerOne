99202a9609a983fa2208cea346fa3835df5d2072
Ahror Jabborov <ahror7j@gmail.com>
2022-06-01 20:50:16 +0300

    Initial Commit

---
+++ primerOne/primerOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  primerOneApp.swift
+//  primerOne
+//
+//  Created by Ahror Jabborov on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct primerOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
